5583ecc1d26e37c0d73ed577f7da46858577f3f8
Amalena <amalena.gautierbonneau@proton.me>
2025-10-30 16:47:15 +0100

    creation .h class pile x river

--- Chantier/pile.h
+++ Chantier/pile.h
@@ -2,9 +2,29 @@
 // Created by barnab on 05/10/2025.
 //
 
-#ifndef PROJETLO21A25_PIOCHE_H
-#define PROJETLO21A25_PIOCHE_H
-class Pioche {
+#ifndef PROJETLO21A25_PILE_H
+#define PROJETLO21A25_PILE_H
+#include <vector>
+using namespace std;
+//using namespace Marilou;
+namespace Marilou {//temporaire
+    class Tile;
+}
+namespace Amalena {
+    class Pile{
+        vector <Marilou::Tile*> tuiles;
 
-};
-#endif //PROJETLO21A25_PIOCHE_H
+        Pile(vector <Marilou::Tile*> t);
+        ~Pile();//friend jeu ou ctrl reflechir si op à faire : désallouer le vecteur
+        Pile(const Pile& p)=delete;//et si retour en arrière?
+        Pile& operator=(Pile& p)=delete;//? utilisation si memo retour arrière
+        Marilou::Tile* Draw();//piocher  ?? creéer une facon dans piocher plusieurs ou utiliser plsrs fois méthode
+    public:
+        bool isEmpty()const;
+        friend class controler;
+        friend class river;
+    };
+}
+//opstream inutile?? on ne va pas afficher la pioche
+// iterator déjà inclu dans vector; memento??
+#endif //PROJETLO21A25_PILE_H
--- Chantier/river.h
+++ Chantier/river.h
@@ -2,7 +2,29 @@
 // Created by barnab on 05/10/2025.
 //
 
-#ifndef PROJETLO21A25_RIVIERE_H
-#define PROJETLO21A25_RIVIERE_H
+#ifndef PROJETLO21A25_RIVER_H
+#define PROJETLO21A25_RIVER_H
+#include <vector>
+#include <iostream>
+using namespace std;
+namespace Marilou {//temporaire
+    class Tile;
+}
+namespace Amalena {
+    class River{
+        vector<Marilou::Tile*> tiles;
 
-#endif //PROJETLO21A25_RIVIERE_H
+        River(vector <Marilou::Tile*> t);//friend avec ctrl?? ou jeu
+        ~River();//friend jeu ou ctrl reflechir si op à faire : désallouer le vecteur
+        River(const River& f)=delete;
+        River& operator=(River& f)=delete;
+    public:
+        Marilou::Tile& giveTile();
+        bool stay1()const;
+        const size_t getPosition(Marilou::Tile* t) const;// et donc par repercussion le cout en pierres
+        //réfléchir à qui gère les transactions + création d'une erreur si pierre insufissantes juste une cdtion
+        //??alimenter s'auto alimente qd reste 1
+    };
+}
+ostream& operator<<(ostream& f, const Amalena::River& r);
+#endif //PROJETLO21A25_RIVER_H
