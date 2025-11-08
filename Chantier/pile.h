65a8d70749ab012fd933673d42b0b02bd78d5c8e
Amalena <amalena.gautierbonneau@proton.me>
2025-11-08 08:48:40 +0100

    m à j .h class pile x river

--- Chantier/pile.h
+++ Chantier/pile.h
@@ -5,22 +5,30 @@
 #ifndef PROJETLO21A25_PILE_H
 #define PROJETLO21A25_PILE_H
 #include <vector>
+#include <string>
 using namespace std;
 //using namespace Marilou;
 namespace Marilou {//temporaire
     class Tile;
 }
 namespace Amalena {
-    class Pile{
-        vector <Marilou::Tile*> tuiles;
+    class PileException
+    {
+        string info;
+    public:
+        PileException(string info):info(info){}
+        const string& getInfo()const{return info;}
 
-        Pile(vector <Marilou::Tile*> t);
+    };
+    class Pile{
+        vector <Marilou::Tile*> tiles;
+        explicit Pile(vector <Marilou::Tile*> t): tiles(t){};
         ~Pile();//friend jeu ou ctrl reflechir si op à faire : désallouer le vecteur
         Pile(const Pile& p)=delete;//et si retour en arrière?
         Pile& operator=(Pile& p)=delete;//? utilisation si memo retour arrière
         Marilou::Tile* Draw();//piocher  ?? creéer une facon dans piocher plusieurs ou utiliser plsrs fois méthode
     public:
-        bool isEmpty()const;
+        bool isEmpty() const;//création d'un getters pour état
         friend class controler;
         friend class river;
     };
