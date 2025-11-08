ee1dec7e6bc0b6976693cf2dfd8dcd7c7bfdcb10
Amalena <amalena.gautierbonneau@proton.me>
2025-11-08 08:51:19 +0100

    m à j .h class pile x river

--- Chantier/pile.cpp
+++ Chantier/pile.cpp
@@ -2,3 +2,26 @@
 // Created by barnab on 05/10/2025.
 //
 #include "pile.h"
+#include <vector>
+using namespace std;
+namespace Amalena
+{
+    Marilou::Tile* Pile::Draw()
+    {
+
+        if (isEmpty()) throw PileException("Empty pile");
+
+        Marilou::Tile* t = tiles.back();
+        tiles.pop_back();
+        return t;
+    }//en admettant que les tuiles ont aléatoirement été placé dans le vecteur
+    //que pour une tuile réfléchir à verif prendre plusieur à la fois
+
+    bool Pile::isEmpty()const
+    {
+        return tiles.empty();
+    }
+
+
+
+}
--- Chantier/river.cpp
+++ Chantier/river.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "river.h"
+#include "pile.h"
+namespace Amalena
+{
+    River::River()
+    {
+        for (int i =0; i<(Game::getNbPlayers()+2);i++){
+            tiles.push_back(Pile::Draw());
+        }
+    }
+
+    Marilou::Tile& River::giveTile(size_t position)
+    {
+
+    }
+
+    bool River::stay1() const
+    {
+        return tiles.size()==1;
+    }
+
+    const size_t River::getPosition(Marilou::Tile* t) const
+    {
+        for (std::vector<unsigned int>::const_iterator it= tiles.begin();it!=t; it++)
+        {
+
+        }
+    }
+}
