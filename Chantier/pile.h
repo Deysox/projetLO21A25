32a63e74d8e8c10bbe777ee577a36c304b66f465
barnab <deysox@laposte.net>
2025-11-16 17:49:24 +0100

    Revert "public constructors"
    This reverts commit 64292322c422127c8abe5aea1bfad6a8f7ca51c8.

--- Chantier/pile.h
+++ Chantier/pile.h
@@ -12,7 +12,14 @@ namespace Marilou {//temporaire
     class Tile;
 }
 namespace Amalena {
+    class PileException
+    {
+        string info;
+    public:
+        PileException(string info):info(info){}
+        const string& getInfo()const{return info;}
 
+    };
     class Pile{
         vector <Marilou::Tile*> tiles;
         explicit Pile(vector <Marilou::Tile*> t): tiles(t){};
@@ -21,9 +28,8 @@ namespace Amalena {
         Pile& operator=(Pile& p)=delete;//? utilisation si memo retour arrière
         Marilou::Tile* Draw();//piocher  ?? creéer une facon dans piocher plusieurs ou utiliser plsrs fois méthode
     public:
-        explicit Pile(vector <Marilou::Tile*> t): tiles(t){};
         bool isEmpty() const;//création d'un getters pour état
-
+        friend class Game;
         friend class river;
     };
 }
--- Chantier/river.h
+++ Chantier/river.h
@@ -15,7 +15,7 @@ using namespace Marilou;
 namespace Amalena {
     class River{
         vector<Tile*> tiles;
-
+        River();//friend avec ctrl?? ou jeu
         ~River();//friend jeu ou ctrl reflechir si op à faire : désallouer le vecteur
         River(const River& f)=delete;
         River& operator=(River& f)=delete;
@@ -25,8 +25,7 @@ namespace Amalena {
         const size_t getPosition(Tile* t) const;// et donc par repercussion le cout en pierres
         //réfléchir à qui gère les transactions + création d'une erreur si pierre insufissantes juste une cdtion
         //??alimenter s'auto alimente qd reste 1
-        River();
-
+        friend class Game;
     };
 }
 ostream& operator<<(ostream& f, const Amalena::River& r);
