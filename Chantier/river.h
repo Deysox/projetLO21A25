a4ae96ed0587741fb870f85607bcda0f3c19ba2c
barnab <deysox@laposte.net>
2025-11-16 17:52:00 +0100

    Revert "add friend game .h class pile x river"
    This reverts commit dd40835f7c495a6d4accb69d262d2877277a90c1.

--- Chantier/river.h
+++ Chantier/river.h
@@ -6,26 +6,23 @@
 #define PROJETLO21A25_RIVER_H
 #include <vector>
 #include <iostream>
-#include "tile.h"
 using namespace std;
-using namespace Marilou;
-//namespace Marilou {//temporaire
-//    class Tile;
-//}
+namespace Marilou {//temporaire
+    class Tile;
+}
 namespace Amalena {
     class River{
-        vector<Tile*> tiles;
+        vector<Marilou::Tile*> tiles;
         River();//friend avec ctrl?? ou jeu
         ~River();//friend jeu ou ctrl reflechir si op à faire : désallouer le vecteur
         River(const River& f)=delete;
         River& operator=(River& f)=delete;
     public:
-        Tile& giveTile(size_t position);
+        Marilou::Tile& giveTile(size_t position);
         bool stay1()const;
-        const size_t getPosition(Tile* t) const;// et donc par repercussion le cout en pierres
+        const size_t getPosition(Marilou::Tile* t) const;// et donc par repercussion le cout en pierres
         //réfléchir à qui gère les transactions + création d'une erreur si pierre insufissantes juste une cdtion
         //??alimenter s'auto alimente qd reste 1
-        friend class Game;
     };
 }
 ostream& operator<<(ostream& f, const Amalena::River& r);
