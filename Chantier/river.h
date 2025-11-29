d0b3e2a27c600a56ce852b3ff49ec2d4275c79db
Amalena <amalena.gautierbonneau@proton.me>
2025-11-29 15:59:19 +0100

    classictile add pile&river

--- Chantier/river.h
+++ Chantier/river.h
@@ -1,34 +1,29 @@
-//
-// Created by barnab on 05/10/2025.
-//
-
 #ifndef PROJETLO21A25_RIVER_H
 #define PROJETLO21A25_RIVER_H
 #include <vector>
 #include <iostream>
 #include "tile.h"
-#include "game.h"
 #include "pile.h"
 using namespace std;
-using namespace Marilou;
+using namespace Barnabe;
 
 
 namespace Amalena {
     class River{
-        vector<Tile*> tiles;
+        vector<ClassicTile*> tiles;
         int max_tiles;
         Pile& pile;
 
-        River(const River& f)=delete;
+        River(const River& r):tiles(r.tiles), max_tiles(r.max_tiles), pile(r.pile){};
         River& operator=(River& f)=delete;
         void fillriver();
     public:
-        Tile& giveTile(size_t position);
+        ClassicTile& giveTile(size_t position);
         bool stay1()const;
-        size_t getPosition(const Tile* t) const;
-        River( size_t nb, Pile& pile);
-        River(const vector<Tile*> tiles, size_t nb, Pile& pile): tiles(tiles), max_tiles(nb), pile(pile){};
-        ~River();;
+        size_t getPosition(const ClassicTile* t) const;
+        River(size_t nb, Pile& pile);
+        River(vector<ClassicTile*>& tiles, size_t nb, Pile& pile): tiles(tiles), max_tiles(nb), pile(pile){};
+        ~River();
     };
 }
 ostream& operator<<(ostream& f, const Amalena::River& r);
