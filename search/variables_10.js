bcb1bafcd8c1adb8eb7416c138fc213897674b25
amale-na <amale-na@users.noreply.github.com>
2025-12-19 21:40:07 +0000

    deploy: c8cbbb0c1428b3351192d5d6de5811aad7c4c36e

--- search/variables_10.js
+++ search/variables_10.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['w_0',['w',['../d0/d4c/classBarnabe_1_1CellQt.html#a53711382e8b44682043a65f717641612',1,'Barnabe::CellQt']]]
+  ['w_0',['w',['../d0/d4c/classBarnabe_1_1CellQt.html#a53711382e8b44682043a65f717641612',1,'Barnabe::CellQt']]],
+  ['window_1',['window',['../d0/d79/classEloise_1_1PlayerQt.html#aaebc065fedc1e3c8ffbf803ffaee9833',1,'Eloise::PlayerQt']]]
 ];
