c8cbbb0c1428b3351192d5d6de5811aad7c4c36e
Amalena <amalena.gautierbonneau@proton.me>
2025-12-19 22:39:40 +0100

    addBoardQT display

--- Joueurs/playerQt.h
+++ Joueurs/playerQt.h
@@ -6,16 +6,17 @@
 #define PROJETLO21A25_PLAYERQT_H
 #include "player.h"
 #include <QObject>
+#include "../Plateau/QT/boardqt.h"
 
 namespace Eloise {
     class PlayerQt : public QObject , public Player {
         Q_OBJECT
     public:
+        BoardQt* BoardWidget=nullptr;
+        QWidget* window =nullptr;
         PlayerQt(const string& s,  QObject* parent = nullptr) : QObject(parent), Player(s) {}
         void displayBoard() override;
         void placeTile(const Tile& tile) override;
-        signals:
-            void boardDisplay(const QString &boardText);
     };
     class ArchitectQt : public PlayerQt {
         int difficulty;
